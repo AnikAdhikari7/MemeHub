44cb0654c0a99c327cd64cdf38016417006e4a67
Anik Adhikari <developwithanik@gmail.com>
2023-02-18 15:13:46 +0530

    v1.1.0 - storage permission sdk>=28

--- app/src/main/java/com/example/memehub/MainActivity.kt
+++ app/src/main/java/com/example/memehub/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.memehub
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
 import android.net.Uri
@@ -9,9 +11,10 @@ import android.provider.MediaStore
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
-import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.android.volley.Request
 import com.android.volley.toolbox.JsonObjectRequest
 import com.bumptech.glide.Glide
@@ -20,11 +23,18 @@ import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.target.Target
 import com.example.memehub.databinding.ActivityMainBinding
-import com.squareup.picasso.Picasso
 
 class MainActivity : AppCompatActivity() {
     private var currentImageUrl: String? = null
+    private var githubRepo = "https://github.com/AnikAdhikari7/MemeHub"
+    private var githubReleases = "Download MemeHub:\nhttps://github.com/AnikAdhikari7/MemeHub/releases"
     private lateinit var binding: ActivityMainBinding
+
+    //variable for permission
+    companion object {
+        private const val STORAGE_PERMISSION_CODE = 100
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,7 +44,7 @@ class MainActivity : AppCompatActivity() {
 
         val actionBar = supportActionBar
         actionBar!!.title = "MemeHub"
-        actionBar.subtitle = "by: github.com/AnikAdhikari7"
+        actionBar.subtitle = "developed by: github.com/AnikAdhikari7"
 
 
         actionBar.setDisplayUseLogoEnabled(true)
@@ -42,6 +52,7 @@ class MainActivity : AppCompatActivity() {
 
 
         memeLoad()
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -54,7 +65,7 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.github_logo -> {
-                val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/AnikAdhikari7/MemeHub"))
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(githubRepo))
                 startActivity(intent)
             }
         }
@@ -103,19 +114,57 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun btShare(view: View) {
-        val bitmapDrawable = binding.ivMemeImage.drawable as BitmapDrawable
-        val bitmap = bitmapDrawable.bitmap
-        val bitmapPath = MediaStore.Images.Media.insertImage(contentResolver, bitmap, "$currentImageUrl", null)
-        val bitmapUri = Uri.parse(bitmapPath)
-        val intent = Intent(Intent.ACTION_SEND)
-        intent.type = "image/*"
-        intent.type = "text/plain"
-        intent.putExtra(Intent.EXTRA_STREAM, bitmapUri)
-        intent.putExtra(Intent.EXTRA_TEXT, "Hey, Checkout this cool meme I got from MemeHub:\n$currentImageUrl")
-        startActivity(Intent.createChooser(intent, "Share image:"))
+
+        try {
+            val bitmapDrawable = binding.ivMemeImage.drawable as BitmapDrawable
+            val bitmap = bitmapDrawable.bitmap
+            val bitmapPath = MediaStore.Images.Media.insertImage(contentResolver, bitmap, "$currentImageUrl", null)
+            val bitmapUri = Uri.parse(bitmapPath)
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "image/*"
+            intent.type = "text/plain"
+            intent.putExtra(Intent.EXTRA_STREAM, bitmapUri)
+            intent.putExtra(Intent.EXTRA_TEXT, "Hey, Checkout this cool meme I got from MemeHub!\n$githubReleases")
+            startActivity(Intent.createChooser(intent, "Share image:"))
+        } catch (e: Exception) {
+            checkPermission(
+                Manifest.permission.WRITE_EXTERNAL_STORAGE, STORAGE_PERMISSION_CODE
+            )
+        }
 
     }
+
+
     fun btNext(view: View) {
         memeLoad()
     }
+
+
+    // Function to check and request permission
+    private fun checkPermission(permission: String, requestCode: Int) {
+        if (ContextCompat.checkSelfPermission(this@MainActivity, permission) == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this@MainActivity, arrayOf(permission), requestCode)
+        }
+    }
+
+
+    // This function is called when the user accepts or decline the permission.
+    // Request Code is used to check which permission called this function.
+    // This request code is provided when the user is prompt for permission.
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == STORAGE_PERMISSION_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this@MainActivity, "Storage Permission Granted", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this@MainActivity, "Storage Permission Denied", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+
 }
